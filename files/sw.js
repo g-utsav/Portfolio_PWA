109bb13909a9225cdaab4b635acd6a2472b19253
Utsav Gupta <gutsav9@gmail.com>
2024-01-19 19:48:54 +0530

    fetch handled

--- files/sw.js
+++ files/sw.js
@@ -2,10 +2,29 @@
 const cacheName = "cache-v1";
 const resourcesToPrecache = [  
     "/Portfolio_PWA/index.html",
-    "/Portfolio_PWA/files/main.js",
-    "/Portfolio_PWA/files/styles.css",
-    "/Portfolio_PWA/files/styles1.css",
-    "/Portfolio_PWA/files/scrollreveal.js"
+"/Portfolio_PWA/files/boxicons.min.css",   
+"/Portfolio_PWA/files/hiberrico.png",
+"/Portfolio_PWA/files/ld.png",
+"/Portfolio_PWA/files/project3.png",
+"/Portfolio_PWA/files/styles.css",
+"/Portfolio_PWA/files/classDiagram.jpeg",  
+"/Portfolio_PWA/files/htmlico.png",
+"/Portfolio_PWA/files/main.js",
+"/Portfolio_PWA/files/project4.png",
+"/Portfolio_PWA/files/styles1.css",
+"/Portfolio_PWA/files/cssico.png",         
+"/Portfolio_PWA/files/images.png",
+"/Portfolio_PWA/files/mysql.png",
+"/Portfolio_PWA/files/scrollreveal.js",
+"/Portfolio_PWA/files/sw.js",
+"/Portfolio_PWA/files/gh.svg",             
+"/Portfolio_PWA/files/javaico.png",
+"/Portfolio_PWA/files/pic10.PNG",
+"/Portfolio_PWA/files/springbootico.png",
+"/Portfolio_PWA/files/h1.png",             
+"/Portfolio_PWA/files/jsico.png",
+"/Portfolio_PWA/files/profile4.png",
+"/Portfolio_PWA/files/springico.png"
 ];
 
 
@@ -25,4 +44,11 @@ self.addEventListener("activate", event =>{
 
 self.addEventListener("fetch", event =>{
     console.log("Fetch Interceptor for ",event.request.url);
+
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+
 });
