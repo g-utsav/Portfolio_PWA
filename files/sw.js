405a73fb7bad8b7c262d1036050813ba9f26ffe3
Utsav Gupta <gutsav9@gmail.com>
2024-01-19 19:24:53 +0530

    Basic SW init

--- files/sw.js
+++ files/sw.js
@@ -1,5 +1,23 @@
+
+const cacheName = "cache-v1";
+const resourcesToPrecache = [
+    "/",
+    "index.html",
+    "/files/main.js",
+    "/files/styles.css",
+    "/files/styles1.css",
+    "/files/scrollreveal.js"
+];
+
+
 self.addEventListener("install", event =>{
     console.log("Install Event");
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+    )
 });
 
 self.addEventListener("activate", event =>{
